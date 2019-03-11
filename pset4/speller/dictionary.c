3da3d8e0dbc0c922bcda0315de02ac47fd68d336
Rose Parker <parker.rose@ymail.com>
2019-03-11 17:23:27 +0000

    passes all tests but it seems to be allocating each word seperately even if a part of a word already is word.  Also memory is not freeing correctly

--- pset4/speller/dictionary.c
+++ pset4/speller/dictionary.c
@@ -67,51 +67,53 @@ bool load(const char *dictionary)
     //Insert words into trie
     while(fscanf(file, "%s", word) != EOF)
     {
-       //printf("%s\n", word);
-
-       //nav->is_word = true;
        wordCount++;
 
         //goes through each character in every word
-       for (int character = 0, wordLength = strlen(word); character < wordLength; character++)
+       for (int letter = 0, wordLength = strlen(word); letter < wordLength; letter++)
        {
            //printf("%c\n", word[position]);
 
            //gets alpha index for each letter at lowercase setting
-            if (isalpha(word[character]))
+            if (isalpha(word[letter]))
             {
-                alphaIndex = tolower(word[character]) - 'a';
+                alphaIndex = tolower(word[letter]) - 'a';
                 //printf("Letter: %c\n", word[character]);
             }
-            if (word[character] == '\'')
+            else if (word[letter] == '\'')
             {
             //alpha index in case there's an apostrophe.  It's a given so I don't have to give specifics on if it's an apostrophe or some other character outside of the alphabet
                 alphaIndex = 26;
             }
 
+
             //ready to insert into trie
             if (nav->children[alphaIndex] == NULL)
             {
-                //printf("alphaIndex: %i\n\n", alphaIndex);
+                //printf("Hello");
                 node *newNode = malloc(sizeof(node));
+                nav->children[alphaIndex] = newNode;
 
                 //sets newNode's child to null
                 for(int j = 0; j < N; j++)
                 {
                     newNode->children[j] = NULL;
                 }
-                nav->children[alphaIndex] = newNode;
+                //printf("alphaIndex: %i\n\n", alphaIndex);
                 nav = newNode;
-                continue;
+
             }
-            else
+
+            else if (nav->children[alphaIndex] != NULL)
             {
+                //printf("Hello");
                 nav = nav->children[alphaIndex];
-
             }
 
-        }
+       }
+
         nav->is_word = true;
+
         nav = root;
     }
 
@@ -128,29 +130,38 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    return wordCount;
+    if (&load)
+    {
+        return wordCount;
+    }
+
+    return 0;
 }
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    //case-insensitive
-    //should only return true for words actually in dictionary
-    //beware hard coding common words like "the"
-    // TODO
     node *trav = root;
-    int txtAlphaIndex = 0;
+
+    int misspelledWord = 0;
 
 
-    for (int textChar = 0, textWord = strlen(word); textChar < textWord; textChar++)
+    // postion = 1, length = 1, temp, string_length;
+    // temp = string_length = strlen(word);
+
+
+
+
+     for (int textChar = 0, textWord = strlen(word); textChar < textWord; textChar++)
     {
+        int txtAlphaIndex = 0;
         //gets alpha index for each letter at lowercase setting
             if (isalpha(word[textChar]))
             {
                 txtAlphaIndex = tolower(word[textChar]) - 'a';
                 //printf("Letter: %c\n", word[character]);
             }
-            if (word[textChar] == '\'')
+            else if (word[textChar] == '\'')
             {
             //alpha index in case there's an apostrophe.  It's a given so I don't have to give specifics on if it's an apostrophe or some other character outside of the alphabet
             txtAlphaIndex = 26;
@@ -160,45 +171,35 @@ bool check(const char *word)
             //compare to words in trie
             if(trav->children[txtAlphaIndex] == NULL)
             {
+                misspelledWord++;
                 return false;
             }
-            else if (trav->children[txtAlphaIndex] != NULL && textChar != textWord -1)
+            else if (trav->children[txtAlphaIndex] != NULL)
             {
                 trav = trav->children[txtAlphaIndex];
             }
-            else if (textChar == textWord -1)
-            {
-                //printf("%s is a word in the dictionary.\n", word);
-                return true;
-            }
     }
-    return false;
+    return trav->is_word;
+}
+
+void freeNode(node *trav)
+{
+    for (int i = 0; i < N; i++)
+    {
+        // Pointer set to children
+        if (trav -> children[i])
+            freeNode(trav -> children[i]);
+
+    }
+    free(trav);
 }
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // // TODO
-    node *cursor = root;
-
-    //travel to last child
-
-    if (cursor->is_word == false)
-    {
-        for (int position = 0; position < N; position++)
-        {
-            if(cursor->children[position] != NULL)
-            {
-                free(cursor->children[position]);
-            }
-
-        }
-    }
-    if (cursor != NULL)
-    {
-        free(cursor);
-        return true;
-    }
-    return false;
+    freeNode(root);
+    return true;
 }
+
+
 
 
